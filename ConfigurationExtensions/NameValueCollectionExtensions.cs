8cc8ab3fa49c28e63392bac2afddb22354fa4607
Tom DuPont <tdupont750@gmail.com>
2015-07-19 22:18:00 -0700

    Initial Commit

---
+++ ConfigurationExtensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace ConfigurationExtensions
+{
+    public static class NameValueCollectionExtensions
+    {
+        private static readonly Type[] SupportedByChangeType =
+        {
+            typeof (string),
+            typeof (DateTime)
+        };
+
+        public static T CreateObject<T>(
+            this NameValueCollection collection,
+            string prefix = null,
+            bool isRequired = false,
+            bool validate = true)
+            where T : new()
+        {
+            var type = typeof(T);
+
+            if (string.IsNullOrWhiteSpace(prefix))
+            {
+                prefix = type.Name;
+            }
+
+            object result;
+            if (TryCreateObject(collection, prefix, type, validate, out result))
+            {
+                return (T)result;
+            }
+
+            if (isRequired)
+            {
+                throw new KeyNotFoundException(prefix + " not found");
+            }
+
+            return Activator.CreateInstance<T>();
+        }
+
+        private static bool TryCreateObject(
+            this NameValueCollection collection,
+            string prefix,
+            Type type,
+            bool validate,
+            out object result)
+        {
+            var exactMatchCount = collection.AllKeys.Count(k => k == prefix);
+            if (exactMatchCount == 1)
+            {
+                return TryGetValue(prefix, type, collection[prefix], out result);
+            }
+
+            if (exactMatchCount > 1)
+            {
+                throw new InvalidOperationException(prefix + " must be unique");
+            }
+
+            var keyInfos = collection.AllKeys
+                .Where(k => k.StartsWith(prefix))
+                .Select(
+                    k =>
+                    {
+                        var collectionIndex = k.IndexOf('[', prefix.Length);
+                        var isCollection = collectionIndex != -1;
+                        var collectionKey = string.Empty;
+
+                        if (isCollection)
+                        {
+                            var closeCollectionIndex = k.IndexOf(']', collectionIndex);
+                            collectionKey = k.Substring(collectionIndex + 1, closeCollectionIndex - collectionIndex - 1);
+                        }
+
+                        return new
+                        {
+                            IsCollection = isCollection,
+                            CollectionKey = collectionKey
+                        };
+                    })
+                .ToList();
+
+            if (keyInfos.Count == 0)
+            {
+                result = null;
+                return false;
+            }
+
+            if (type.IsGenericType && keyInfos.All(k => k.IsCollection))
+            {
+                var genericType = type.GetGenericTypeDefinition();
+                var genericArguments = type.GetGenericArguments();
+                var distinctCollectionKeys = keyInfos
+                    .Select(k => k.CollectionKey)
+                    .Distinct()
+                    .ToList();
+
+                if (genericType == typeof(List<>))
+                {
+                    var list = (IList)Activator.CreateInstance(type);
+                    var valueType = genericArguments.Single();
+
+                    foreach (var collectionKey in distinctCollectionKeys.OrderBy(int.Parse))
+                    {
+                        var valuePrefix = string.Format("{0}[{1}]", prefix, collectionKey);
+
+                        object valueResult;
+                        if (!TryCreateObject(collection, valuePrefix, valueType, validate, out valueResult))
+                        {
+                            valueResult = GetDefault(valueType);
+                        }
+
+                        list.Add(valueResult);
+                    }
+
+                    result = list;
+                    return true;
+                }
+
+                if (genericType == typeof(Dictionary<,>))
+                {
+                    var keyType = genericArguments.First();
+                    if (keyType != typeof(string))
+                    {
+                        throw new InvalidOperationException(prefix + " must be Dictionary with string key");
+                    }
+
+                    var dictionary = (IDictionary)Activator.CreateInstance(type);
+                    var valueType = type.GetGenericArguments().Skip(1).Single();
+
+                    foreach (var collectionKey in distinctCollectionKeys)
+                    {
+                        var valuePrefix = string.Format("{0}[{1}]", prefix, collectionKey);
+
+                        object valueResult;
+                        if (!TryCreateObject(collection, valuePrefix, valueType, validate, out valueResult))
+                        {
+                            valueResult = GetDefault(valueType);
+                        }
+
+                        dictionary[collectionKey] = valueResult;
+                    }
+
+                    result = dictionary;
+                    return true;
+                }
+
+                throw new NotSupportedException(prefix + " must be of type List<> or Dictionary<,>");
+            }
+
+            result = Activator.CreateInstance(type);
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty);
+
+            foreach (var property in properties)
+            {
+                var propertyPrefix = prefix + "." + property.Name;
+
+                object value;
+                if (TryCreateObject(collection, propertyPrefix, property.PropertyType, validate, out value))
+                {
+                    property.SetValue(result, value);
+                }
+            }
+
+            if (validate)
+            {
+                var context = new ValidationContext(result);
+                Validator.ValidateObject(result, context, true);
+            }
+
+            return true;
+        }
+
+        private static bool TryGetValue(
+            string prefix,
+            Type type,
+            string value,
+            out object result)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                result = null;
+                return false;
+            }
+
+            if (type == typeof(TimeSpan))
+            {
+                result = TimeSpan.Parse(value);
+                return true;
+            }
+
+            if (type.IsEnum)
+            {
+                result = Enum.Parse(type, value);
+                return true;
+            }
+
+            if (type.IsPrimitive || SupportedByChangeType.Contains(type))
+            {
+                result = Convert.ChangeType(value, type);
+                return true;
+            }
+
+            var underlyingType = Nullable.GetUnderlyingType(type);
+            if (underlyingType == null)
+            {
+                throw new NotSupportedException(prefix + " is not a convertable type");
+            }
+
+            return TryGetValue(prefix, underlyingType, value, out result);
+        }
+
+        private static object GetDefault(Type type)
+        {
+            return type.IsValueType
+                ? Activator.CreateInstance(type)
+                : null;
+        }
+    }
+}
---
+++ ConfigurationExtensions/NameValueCollectionExtensionsTests.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using Xunit;
+
+namespace ConfigurationExtensions
+{
+    public class NameValueCollectionExtensionsTests
+    {
+        internal enum TestEnum
+        {
+            Goodnight,
+            Moon
+        }
+
+        [Fact]
+        public void CreateSimpleObject()
+        {
+            var result = ConfigurationManager.AppSettings.CreateObject<TestConfig>();
+
+            Assert.NotNull(result);
+            Assert.Equal(1, result.Int);
+            Assert.Equal(42, result.IntWithDefault);
+            Assert.Equal("Hello", result.String);
+            Assert.Equal(new DateTime(2015, 1, 2), result.DateTime);
+            Assert.Equal(TimeSpan.FromMinutes(1), result.TimeSpan);
+            Assert.Equal(TestEnum.Moon, result.Enum);
+            Assert.Equal(2, result.NullableIntA);
+            Assert.Null(result.NullableIntB);
+        }
+
+        [Fact]
+        public void CreateComplexObject()
+        {
+            var collection = new NameValueCollection
+            {
+                {"TestConfig.Test.Int", "3"},
+                {"TestConfig.ListOfInt[0]", "4"},
+                {"TestConfig.ListOfInt[1]", "5"},
+                {"TestConfig.ListOfTestConfig[2].Int", "6"},
+                {"TestConfig.ListOfTestConfig[2].String", "Arg"},
+                {"TestConfig.ListOfTestConfig[1].Int", "7"},
+                {"TestConfig.MapOfInt[Hello]", "8"},
+                {"TestConfig.MapOfInt[World]", "9"},
+            };
+
+            var result = collection.CreateObject<TestConfig>();
+
+            Assert.NotNull(result);
+
+            Assert.NotNull(result.Test);
+            Assert.Equal(3, result.Test.Int);
+
+            Assert.NotNull(result.ListOfInt);
+            Assert.Equal(2, result.ListOfInt.Count);
+            Assert.Equal(4, result.ListOfInt[0]);
+            Assert.Equal(5, result.ListOfInt[1]);
+
+            Assert.NotNull(result.ListOfTestConfig);
+            Assert.Equal(2, result.ListOfTestConfig.Count);
+            Assert.NotNull(result.ListOfTestConfig[0]);
+            Assert.Equal(7, result.ListOfTestConfig[0].Int);
+            Assert.NotNull(result.ListOfTestConfig[1]);
+            Assert.Equal(6, result.ListOfTestConfig[1].Int);
+            Assert.Equal("Arg", result.ListOfTestConfig[1].String);
+
+            Assert.NotNull(result.MapOfInt);
+            Assert.Equal(2, result.MapOfInt.Count);
+            Assert.Equal(8, result.MapOfInt["Hello"]);
+            Assert.Equal(9, result.MapOfInt["World"]);
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("10")]
+        public void CreateValidObject(string value)
+        {
+            var collection = new NameValueCollection
+            {
+                {"TestConfigWithAnnotations.Int", value},
+            };
+
+            collection.CreateObject<TestConfigWithAnnotations>();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("0")]
+        [InlineData("11")]
+        public void CreateInvalidObject(string value)
+        {
+            var collection = new NameValueCollection
+            {
+                {"TestConfigWithAnnotations.Int", value},
+            };
+
+            Assert.Throws<ValidationException>(
+                () =>
+                {
+                    collection.CreateObject<TestConfigWithAnnotations>();
+                });
+        }
+
+        [Fact]
+        public void CreateInt()
+        {
+            var collection = new NameValueCollection
+            {
+                {"A", "1"},
+            };
+
+            var result = collection.CreateObject<int>("A");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void CreateIntList()
+        {
+            var collection = new NameValueCollection
+            {
+                {"A[1]", "1"},
+                {"A[2]", "2"},
+                {"Something", "Else"},
+                {"A[4]", "4"},
+            };
+
+            var result = collection.CreateObject<List<int>>("A");
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+            Assert.Equal(1, result[0]);
+            Assert.Equal(2, result[1]);
+            Assert.Equal(4, result[2]);
+        }
+
+        [Fact]
+        public void CreateObjectList()
+        {
+            var collection = new NameValueCollection
+            {
+                {"Something", "Else"},
+                {"TestConfig.ListOfTestConfig[2].Int", "6"},
+                {"TestConfig.ListOfTestConfig[2].String", "Arg"},
+                {"TestConfig.ListOfTestConfig[1].Int", "7"},
+            };
+
+            var result = collection.CreateObject<TestConfig>();
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.ListOfTestConfig);
+            Assert.Equal(2, result.ListOfTestConfig.Count);
+            Assert.NotNull(result.ListOfTestConfig[0]);
+            Assert.Equal(7, result.ListOfTestConfig[0].Int);
+            Assert.NotNull(result.ListOfTestConfig[1]);
+            Assert.Equal(6, result.ListOfTestConfig[1].Int);
+            Assert.Equal("Arg", result.ListOfTestConfig[1].String);
+        }
+
+        [Fact]
+        public void CreateIntDictionary()
+        {
+            var collection = new NameValueCollection
+            {
+                {"A[A]", "1"},
+                {"Something", "Else"},
+                {"A[BB]", "22"},
+                {"A[CCC]", "333"},
+            };
+
+            var result = collection.CreateObject<Dictionary<string, int>>("A");
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+            Assert.Equal(1, result["A"]);
+            Assert.Equal(22, result["BB"]);
+            Assert.Equal(333, result["CCC"]);
+        }
+
+        [Fact]
+        public void CreateObjectDictionary()
+        {
+            var collection = new NameValueCollection
+            {
+                {"A[A].Int", "1"},
+                {"Something", "Else"},
+                {"A[BB].String", "Hello"},
+                {"A[CCC].Enum", "Goodnight"},
+            };
+
+            var result = collection.CreateObject<Dictionary<string, TestConfig>>("A");
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+            Assert.Equal(1, result["A"].Int);
+            Assert.Equal("Hello", result["BB"].String);
+            Assert.Equal(TestEnum.Goodnight, result["CCC"].Enum);
+        }
+
+        internal class TestConfig
+        {
+            public TestConfig()
+            {
+                IntWithDefault = 42;
+            }
+
+            public int Int { get; set; }
+            public int IntWithDefault { get; set; }
+            public string String { get; set; }
+            public DateTime DateTime { get; set; }
+            public TimeSpan TimeSpan { get; set; }
+            public TestEnum Enum { get; set; }
+            public int? NullableIntA { get; set; }
+            public int? NullableIntB { get; set; }
+            public TestConfig Test { get; set; }
+            public List<int> ListOfInt { get; set; }
+            public List<TestConfig> ListOfTestConfig { get; set; }
+            public Dictionary<string, int> MapOfInt { get; set; }
+        }
+
+        internal class TestConfigWithAnnotations
+        {
+            [Range(1, 10)]
+            public int Int { get; set; }
+        }
+    }
+}
